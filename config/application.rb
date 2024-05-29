acb17e03e5273f159038e89bf8515d5e885b4a8b
rem beuch <beucherie.r@gmail.com>
2024-05-29 19:25:54 +0200

    Configure request front

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,13 @@ module RailsBackend
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://triple-triad-front.vercel.app'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
+
     config.api_only = true
   end
 end
